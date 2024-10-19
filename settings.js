54125a51465c8c350d99ae130a9c99cad56370d0
Nonamess1 <Dctrixx@gmail.com>
2024-10-19 13:58:58 +0200

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,9 +1,9 @@
 const settings = {
   packname: 'My Sticker Pack',
   author: 'My Bot',
-  botName: "Knight Bot",
-  botOwner: 'Professor', // Your name
-  ownerNumber: '917023951514', //Your number
+  botName: "ㆈㆄㆈBot",
+  botOwner: 'mm7', // Your name
+  ownerNumber: '4915752839661', //Your number
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
